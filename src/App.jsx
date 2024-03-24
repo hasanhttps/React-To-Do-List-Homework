85fb6a4debe122c94129ab30896d7df9e733096a
Hasan Abdullazade <hasanabdullazad@gmail.com>
2024-03-24 18:37:20 +0400

    Latest Version 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faTrash, faXmark } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
 
+  const [index, setIndex] = useState(-1);
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [onEdit, setOnEdit] = useState(false);
+  const [started, setStarted] = useState(false);
   const [newTaskDesc, setNewTaskDesc] = useState('');
 
   const handleAdd = () => {
-    if (newTask != '' && newTaskDesc != '') {
+    if (newTask != '' && newTaskDesc != '' && !onEdit) {
       setTasks([...tasks, { title : newTask, description : newTaskDesc} ]);
+      setNewTask('');
+      setNewTaskDesc('');
+    }
+    else if (onEdit && newTask != '' && newTaskDesc != ''){
+      setTasks(tasks.map((task, i) => {
+        if (i == index) {
+          task.title = newTask;
+          task.description = newTaskDesc;
+        }
+        return task;
+      }))
+    }
+  }
+
+  const handleRemove = (index) => {
+    setTasks(tasks.filter((value, i) => (i != index)))
+  }
+
+  const handleEdit = (index) => {
+    if (!onEdit){
+      setOnEdit(true);
+      setIndex(index);
+      setNewTask(tasks[index].title);
+      setNewTaskDesc(tasks[index].description);
+    }
+    else{
+      setOnEdit(false);
+      setIndex(-1);
       setNewTask('');
       setNewTaskDesc('');
     }
   }
 
   useEffect(() => {
-    if (tasks.length == 0)
+    if (!started){
       setTasks(JSON.parse(localStorage.getItem('tasks')));
+      setStarted(true);
+    }
     else 
       localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks]);
@@ -31,29 +64,29 @@ function App() {
       
       <div className='todolist'>
         <div className="container" style={{width:400, justifyContent: 'center', position: 'initial'}}>
-            <input className="searchbar" style={{width:400}} onChange={(e) => setNewTask(e.target.value)} type="text" placeholder={"Add a task name."}></input>
+            <input className="searchbar" value={newTask} style={{width:400}} onChange={(e) => setNewTask(e.target.value)} type="text" placeholder={"Add a task name."}></input>
         </div>
         <div className="container">
-            <input className="searchbar" onChange={(e) => setNewTaskDesc(e.target.value)} type="text" placeholder={"Add a task description."}></input>
+            <input className="searchbar" value={newTaskDesc} onChange={(e) => setNewTaskDesc(e.target.value)} type="text" placeholder={"Add a task description."}></input>
             <button onClick={handleAdd} className="searchbutton">Confirm</button>
         </div>
       </div>
 
       <div className='tasks'>
-        <p className='tasksLabel'>Tasks</p>
-        { tasks.map((task, index) => (
-          <div className='task' key={index}>
+        { tasks.length == 0 ? '' : <p className='tasksLabel'>Tasks</p>}
+        { tasks.map((task, i) => (
+          <div className='task' key={i}>
             <div className='left-section'>
               <p className='task-title'>{task.title}</p>
               <p className='task-desc'>{task.description}</p>
             </div>
 
             <div className='right-section'>
-              <button id='button' className='editBt'>
-                <FontAwesomeIcon style={{height: '16px'}} icon={faPen} />
+              <button id='button' onClick={() => handleEdit(i)} className='editBt'>
+                {onEdit && i == index ? <FontAwesomeIcon style={{height: '16px'}} icon={faXmark}/> : <FontAwesomeIcon style={{height: '16px'}} icon={faPen} />}
               </button>
 
-              <button id='button' className='removeBt'>
+              <button id='button' onClick={() =>  handleRemove(i)} className='removeBt'>
                 <FontAwesomeIcon style={{height: '16px'}} icon={faTrash} />
               </button>
             </div>
